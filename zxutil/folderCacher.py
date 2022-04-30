a677626f34dabf3813194951587b2c1b5726c822
ZackaryW <zackaryxcwang@gmail.com>
2022-04-30 14:20:23 -0700

    !feat: revamped folderCacher

--- zxutil/folderCacher.py
+++ zxutil/folderCacher.py
@@ -1,151 +1,227 @@
-
+import logging
 import os
-import shutil
-from PIL import Image
+import typing
 import json
-
-
+import PIL
+from PIL import Image
+from PIL.Image import Image as PILImage
+import requests
 # ANCHOR load methods
-def load_json(file_path : str, **kwargs) -> str:
-    with open(file_path, "r") as file:
-        return json.load(file)
-
-def load_file(file_path : str, **kwargs) -> str:
-    with open(file_path, "r") as file:
+def load_file(path : str, **kwargs) -> str:
+    with open(path, "r") as file:
         return file.readlines()
 
-def load_image(file_path : str, **kwargs) -> str:
-    return Image.open(file_path)
+def save_file(obj, path : str, **kwargs):
+    with open(path, "w") as file:
+        file.write(obj)
 
-def save_file(raw, file_path : str, **kwargs):
-    with open(file_path, "w") as file:
-        file.write(raw)
+def load_json(path : str, **kwargs) -> str:
+    with open(path, "r") as file:
+        return json.load(file)
 
-def save_image(raw, file_path : str, **kwargs):
-    raw : Image
-    raw.save(file_path)
+def save_json(obj, path : str, **kwargs):
+    with open(path, "w") as file:
+        json.dump(obj, file)
 
-def save_json(raw, file_path : str, **kwargs):
-    with open(file_path, "w") as file:
-        json.dump(raw, file)
+def fetch_web_img(link : str, **kwargs) -> PILImage:
+    if link is None:
+        return
+    res = requests.get(link)
+    return res.content
+
+def save_web_img(obj, path : str, **kwargs):
+    if isinstance(obj, PILImage):
+        obj.save(path)
+        return
+    if obj is None:
+        return
+    with open(path, "wb") as file:
+        file.write(obj)
+
+def load_web_img(path : str, **kwargs) -> PILImage:
+    return Image.open(path)
 
 class FolderCacher:
-    """
-    a very primitive way to cache files
+    class _CACHE_FLAG_: pass
+    class CONSTANT_CACHE(_CACHE_FLAG_): pass
+    class HOLDING_CACHE(_CACHE_FLAG_): pass
 
-    """
-    def __init__(self, path : str, create: bool = False) -> None:
-        if create and not os.path.exists(path):
+    def __init__(self, path : str, create_if_null : bool = True, extension : str =None) -> None:
+        if create_if_null and not os.path.exists(path):
             os.mkdir(path)
-
-        if not os.path.exists(path):
+        elif not os.path.exists(path):
             raise FileNotFoundError(path)
 
-        self.most_frequent_files = {} 
-        self.minimum_needed_to_cache = 10
-        self.maximum_cache_size = 30
-        self.holding_queue = {}
-        self.holding_queue_size = 5
-        self.file_load_method = load_file
-        self.file_save_method = save_file
+        self._constant_cache = {}
+        self._holding_queue = {}
+        self._counting_index = {}
+        
+        self.method_fetch = None
+        self.method_load = None
+        self.method_save = None
 
-        self.count_index = {}
-        self.path = path
-        # walk path
+        self.MAX_HOLDING_QUEUE_SIZE = 30
+        self.MAX_CONSTANT_CACHE_SIZE = 30
+        self.MIN_REQ_TO_PLACE_CONSTANT = 10
+
+        self.GLOBAL_EXTENSION = extension
+        self.PATH = path
+
+        # list path
         files = os.listdir(path)
         for file in files:
-            self.count_index[file] = 0, os.path.join(path, file)
-
-
-    def __contains__(self, file_name : str) -> bool:
-        return file_name in self.count_index
+            # split extension
+            file_name, extension = os.path.splitext(file)
+            extension = extension[1:]
+            if extension != self.GLOBAL_EXTENSION and self.GLOBAL_EXTENSION is not None:
+                continue
+            if self.GLOBAL_EXTENSION is None:
+                self._counting_index_action(file)
+            else:
+                self._counting_index_action(file_name)
     
-    def _trigger_count(self, file_name : str, save :bool = False) -> None:
-        if save and file_name not in self.count_index:
-            self.count_index[file_name] = list([0, None])
-
-        elif file_name not in self.count_index:
-            return
-        self.count_index[file_name][0] += 1
-    
-    def _get_holding_queue(self, file_name : str) -> str:
-        if file_name not in self.holding_queue:
-            return None
-        return self.holding_queue[file_name]
-
-    def _get_frequenct(self, file_name : str) -> int:
-        if file_name not in self.count_index:
-            return None
-        return self.most_frequent_files[file_name]
-
-    def _place_holding_queue(self, item, file_name : str):
-        while len(self.holding_queue) >= self.holding_queue_size:
-            # pop first 
-            key = self.holding_queue.popitem()
-        self.holding_queue[file_name] = item
-    
-    def _handle_frequent_files(self, loaded, file_name :str):
-        if self.count_index[file_name][0] < self.minimum_needed_to_cache:
-            return
+    def __contains__(self, name) -> bool:
+        if isinstance(name, str):
+            return name in self._counting_index
+        if isinstance(name, typing.Iterable) and len(name) == 2 and name[0] in self._counting_index:
+            if name[1] == FolderCacher.CONSTANT_CACHE:
+                return name[0] in self._constant_cache
+            if name[1] == FolderCacher.HOLDING_CACHE:
+                return name[0] in self._holding_queue
         
-        if len(self.most_frequent_files) < self.maximum_cache_size:
-            self.most_frequent_files[file_name] = loaded
-            return
-
-
-        current_min_key = file_name
-        current_min_count = self.count_index[file_name][0]
-        for k, v in self.most_frequent_files:
-            if self.count_index[k][0] < current_min_count:
-                current_min_key = k
-                current_min_count = self.count_index[k][0]
+        raise TypeError(f"{name} is not a valid key to check contains")
             
-        if current_min_key == file_name:
-            return
+
+    def _get_from_cache(self, key : str):
+        if key not in self._counting_index:
+            raise KeyError(f"{key} not in index")
         
-        self.most_frequent_files[file_name] = loaded
-        del self.most_frequent_files[current_min_key]
+        if key in self._constant_cache:
+            self._counting_index_action(key)
+            return self._constant_cache[key]
+        if key in self._holding_queue:
+            self._counting_index_action(key)
+            return self._holding_queue[key]
+        return None
 
-
-    def load(self, file_name : str, **kwargs):
-        file_name = str(file_name)
-        self._trigger_count(file_name)
-        if (holding:= self._get_holding_queue(file_name)) is not None:
-            return holding
-        if (item := self._get_frequenct(file_name)) is not None:
-            return item  
-
-        file_path = self.count_index[file_name][1]
-        loaded = self.file_load_method(file_path, **kwargs)
-        self._place_holding_queue(loaded, file_name)
-        self._handle_frequent_files(loaded, file_name)
-
-        return loaded
-        
-
-    def save(self, raw, file_name : str, extension : str=None, overwrite : bool = False,**kwargs):
-        file_name = str(file_name)
-        self._trigger_count(file_name, save=True)
-        complete_save_path = os.path.join(self.path, file_name)
-        if extension is not None:
-            complete_save_path = complete_save_path + "." + extension
-        self._place_holding_queue(raw, file_name, **kwargs)
-        if os.path.exists(complete_save_path) and not overwrite:
-            return
-        self.count_index[file_name][1] = complete_save_path
-        self.file_save_method(raw, complete_save_path)
-
-    def remove_path(self):
-        shutil.rmtree(self.path)
-
-
+    def _counting_index_action(self, key : str):
+        if key not in self._counting_index:
+            self._counting_index[key] = 0
+        else:
+            self._counting_index[key] += 1
     
-    # ANCHOR class
+    def _get_lowest_count_in_holding_cache(self, target_dict :typing.Dict[str, typing.Any]) -> str:
+        if len(target_dict) == 0:
+            return None, None
+    
+        lowest = None
+        for key in target_dict:
+            if lowest is None:
+                lowest = key
+                continue
+            if self._counting_index[key] < self._counting_index[lowest]:
+                lowest = key
 
-    @classmethod
-    def image_cacher(cls, path : str, create :bool = False) -> 'FolderCacher':
-        cacher = FolderCacher(path, create)
-        cacher.file_load_method = load_image
-        cacher.file_save_method = save_image
-        return cacher
+        if lowest is None:
+            return None, 0
 
+        return lowest, self._counting_index[lowest]
+
+
+    def _place_cache(self, key : str, obj : object):
+        # constant cache maintain
+        lowest = None
+        while len(self._constant_cache) > self.MAX_CONSTANT_CACHE_SIZE:
+            # get the lowest on counting index
+            lowest, lowest_count = self._get_lowest_count_in_holding_cache(self._constant_cache)
+            lowest_v = self._constant_cache.pop(lowest)
+            # add lowest 
+            self._holding_queue[lowest] = lowest_v
+
+        # holding queue once over limit, just pop the oldest one
+        while len(self._holding_queue) > self.MAX_HOLDING_QUEUE_SIZE:
+            self._holding_queue.popitem()
+
+        if key in self._constant_cache:
+            return
+
+        if key in self._holding_queue and self._counting_index[key] >= self.MIN_REQ_TO_PLACE_CONSTANT:
+            self._holding_queue.pop(key)
+            self._constant_cache[key] = obj
+            return
+
+        if key not in self._holding_queue:
+            self._holding_queue[key] = obj
+
+    def save(self, key : str,obj=None, **kwargs):
+        if key in self._counting_index:
+            logging.info(f"{key} already exist in cache")
+
+        if self.method_fetch is None:
+            logging.info("No method to fetch")
+            return None
+
+        if obj is None and self.method_fetch is not None:
+            logging.info(f"obj is None, attempting to fetch")
+            obj = self.method_fetch(**kwargs)
+            
+        if obj is None:
+            return None
+        
+        if self.method_save is None:
+            logging.info("No method to save")
+            return obj
+
+        path = os.path.join(self.PATH, key)
+        if self.GLOBAL_EXTENSION is not None:
+            path += f".{self.GLOBAL_EXTENSION}"
+
+        self.method_save(obj=obj, path=path, **kwargs)
+
+        self._counting_index_action(key)
+        self._place_cache(key, obj)
+        return obj
+
+
+    def load(self, key : str, **kwargs):
+        if (cache:=self._get_from_cache(key)) is not None:
+            self._place_cache(key, cache)
+            return cache
+
+        if key not in self._counting_index:
+            logging.info(f"{key} not in index")
+            return None
+
+        if self.method_load is None:
+            logging.info("No method to load")
+            return None
+
+        file_path = os.path.join(self.PATH, f"{key}")
+        if self.GLOBAL_EXTENSION is not None:
+            file_path += "."
+            file_path += self.GLOBAL_EXTENSION
+
+        obj = self.method_load(path=file_path,**kwargs)
+        if obj is None:
+            logging.info(f"{key} load returned None")
+            return None
+
+        self._counting_index_action(key)
+        self._place_cache(key, obj)
+        return obj
+
+    @staticmethod
+    def make_pilimage_cache(path : str, create_if_null : bool = True, extension : str =None) -> 'FolderCacher':
+        cache = FolderCacher(path, create_if_null, extension)
+        cache.method_load = lambda **kwargs: PIL.Image.open(kwargs['path'])
+        cache.method_save = lambda **kwargs: kwargs['obj'].save(kwargs['path'])
+        return cache
+
+    @staticmethod
+    def make_webimg_cache(path : str, create_if_null : bool = True, extension : str ='png') -> 'FolderCacher':
+        cache = FolderCacher(path, create_if_null, extension)
+        cache.method_load = load_web_img
+        cache.method_save = save_web_img
+        cache.method_fetch = fetch_web_img
+
+        return cache
