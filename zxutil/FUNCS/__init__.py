82fcb4e7558739956152c8357849f4f8713a6b2b
ZackaryW <zackaryxcwang@gmail.com>
2022-04-29 21:18:46 -0700

    feat: add parse dimension string

--- zxutil/FUNCS/__init__.py
+++ zxutil/FUNCS/__init__.py
@@ -43,7 +43,33 @@ def is_jsonable(x):
         return False
 
 def get_calling_method_name() -> str:
+    """ 
+    returns the name of the calling method
+
+    Returns:
+        str: name of the calling method
+    """
     curframe = inspect.currentframe()
     calframe = inspect.getouterframes(curframe, 2)
     calling_method_name = calframe[1][3]
     return calling_method_name
+
+def parse_dimension_string(dimension_string : str) -> list:
+    """
+    parses a dimension string [int]xint to x,y
+    """
+    dimension_string = dimension_string.lower()
+
+    if "x" not in dimension_string:
+        return None
+
+    splits = dimension_string.split("x")
+    if len(splits) != 2:
+        return None
+    
+    try:
+        x = int(splits[0])
+        y = int(splits[1])
+        return x, y
+    except ValueError:
+        return None
