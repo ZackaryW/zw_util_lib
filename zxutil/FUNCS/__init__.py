796d93b234f5c9b70d7b25cabbe3153f0344a84e
ZackaryW <zackaryxcwang@gmail.com>
2022-06-19 21:52:21 -0700

    feat: update funcs

--- zxutil/FUNCS/__init__.py
+++ zxutil/FUNCS/__init__.py
@@ -1,5 +1,5 @@
 import datetime
-
+import time
 def create_timestamp() -> int:
     """
     creates a unix timestamp
@@ -8,3 +8,17 @@ def create_timestamp() -> int:
     # convert now to unix timestamp milliseconds
     unix_timestamp = int(now.timestamp() * 1000)
     return unix_timestamp
+
+def random_sleep(min=10, max=30, auto_increment_max = True):
+    """
+    Sleeps for a random amount of time
+    """
+    import random
+    if max < min and not auto_increment_max:
+        raise ValueError("max must be greater than min")
+
+    if max < min and auto_increment_max:
+        max = min + max
+
+    sleep_time = random.randint(min, max)
+    time.sleep(sleep_time)
